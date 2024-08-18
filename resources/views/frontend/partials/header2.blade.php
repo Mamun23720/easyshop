b8ab991d78d50e67906225a957528ad3c0282d9e
Mamun23720 <abdalmamun2000@gmail.com>
2024-08-19 01:41:01 +0600

    new work master 2

---
+++ resources/views/frontend/partials/header2.blade.php
@@ -0,0 +1,51 @@
+<header class="header">
+<div class="container-fluid">
+<div class="row">
+<div class="col-xl-3 col-lg-2">
+<div class="header__logo">
+<a href=""><img src="" alt="Logo" ></a>
+</div>
+</div>
+<div class="col-xl-6 col-lg-7">
+<nav class="header__menu">
+<ul>
+<li><a href="#">Home</a></li>
+<li><a href="#">Pages</a>
+<ul class="dropdown">
+<li><a href="#">All Products</a></li>
+<li><a href="#">Shopping Cart</a></li>
+<li><a href="#">Checkout</a></li>
+<li><a href="#">Blog Details</a></li>
+</ul>
+</li>
+<li><a href="#">Blog</a></li>
+<li><a href="#">Contact</a></li>
+</ul>
+</nav>
+</div>
+<div class="col-lg-3">
+<div class="header__right">
+<div class="btn btn-primary">
+<a href="#login">Login</a>
+<a href="#register">Register</a>
+</div>
+<ul class="header__right__widget">
+<li><a href="#"><span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+  <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+</svg></span>
+<div class="tip">0</div>
+</a></li>
+<li><a href="#"><span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+</svg></span>
+<div class="tip">0</div>
+</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="canvas__open">
+<i class="fa fa-bars"></i>
+</div>
+</div>
+</header>
